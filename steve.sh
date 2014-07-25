2d42ac4af13c591969ef80d6855368f16dfd1825
Steve Bennett <stevage@gmail.com>
2014-07-25 13:59:59 +1000

    doc

--- steve.sh
+++ steve.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
+# Run this locally to copy and run the actual scripts on the server.
+
 if [ "$1" = "" ]; then echo "Usage: stevescript.sh hostname"; exit; fi
 for host in $@; do
 scp ~/Dropbox/dev/stevescript/base.sh $host:
